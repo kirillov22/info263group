f4ab8a359007773aa34aec21cf3b9d5f058d1a30
Kirill Kirillov <kki43@uclive.ac.nz>
2017-10-11 12:16:58 +1300

    Made map load on Auckland at start

--- scripts/map.js
+++ scripts/map.js
@@ -16,7 +16,7 @@ var infomap;
 
 
 function initMap(){
-	var myLatlng = {lat: 36.84738,lng: 174.76173}; 
+	var myLatlng = {lat: -36.84738,lng: 174.76173}; 
 	
     infomap = new google.maps.Map(document.getElementById('map'), {
       zoom: 11,
@@ -26,40 +26,40 @@ function initMap(){
 
 	
 	setInterval(APIquery,30000);
-  }
+}
 
-	function newmarker(x,y,name,route) {
+function newmarker(x,y,name,route) {
+
+	locations.push([name,x,y,route]);
+	var myLatlng = {lat: x,lng: y}; 
 	
-		locations.push([name,x,y,route]);
-		var myLatlng = {lat: x,lng: y}; 
-		
-		mark = new google.maps.Marker({
-			position: myLatlng,
-			map: infomap,
-			icon: "media/bus.png",
-			title: 'Double click to zoom'
-		});
-		
-		var binfo = notify(mark.getPosition());
-		var iw = new google.maps.InfoWindow({
-			content: binfo
-		});
-		
-		allmarkers.push(mark);
-		
-		mark.addListener('click', function() {
-			iw.open(infomap, this);
-		});
-		
-		mark.addListener('dblclick', function() {
-			infomap.setZoom(15);
-			infomap.setCenter(this.getPosition());
-		});
-		
-		
+	mark = new google.maps.Marker({
+		position: myLatlng,
+		map: infomap,
+		icon: "media/bus.png",
+		title: 'Double click to zoom'
+	});
+	
+	var binfo = notify(mark.getPosition());
+	var iw = new google.maps.InfoWindow({
+		content: binfo
+	});
+	
+	allmarkers.push(mark);
+	
+	mark.addListener('click', function() {
+		iw.open(infomap, this);
+	});
+	
+	mark.addListener('dblclick', function() {
+		infomap.setZoom(15);
+		infomap.setCenter(this.getPosition());
+	});
+	
+	
 	var autosize = document.getElementById("autosizecb").checked;
 	if (autosize) {
-
+	
 		var bounds = new google.maps.LatLngBounds();
 			
 		for (var i = 0; i < allmarkers.length; i++) {
@@ -67,12 +67,10 @@ function initMap(){
 		}
 		infomap.fitBounds(bounds);
 	}
-		
-	}
+}
 
 function recievebuses(inputparam){
 	for (var busnum = 0; busnum < inputparam.length; busnum++){
-			console.log(inputparam[busnum]);
 	}
 }
 	
@@ -88,20 +86,20 @@ function refreshMap(){
 	// please replace the following example code with by creating a for loop that adds new markers to the map.
 }
 	
-	function notify(loc) {
+function notify(loc) {
+	
+	var name;
+	var route;
+	var testLatlng;
+	
+	for (var i = 0; i < locations.length; i ++) {
+	
+		testLatlng = [locations[i][1], locations[i][2]];
 		
-		var name;
-		var route;
-		var testLatlng;
-		
-		for (var i = 0; i < locations.length; i ++) {
-		
-			testLatlng = [locations[i][1], locations[i][2]];
-			
-			if ((testLatlng[0].toString()).slice(0, 9) == (loc.toString()).slice(1, 10)){
-			name = locations[i][0];
-			route = locations[i][3];
-			}
+		if ((testLatlng[0].toString()).slice(0, 9) == (loc.toString()).slice(1, 10)){
+		name = locations[i][0];
+		route = locations[i][3];
 		}
-		return "<b>"+name + "</b> <br>" + route + "<br>coordinates: " + loc;
 	}
+	return "<b>"+name + "</b> <br>" + route + "<br>coordinates: " + loc;
+}
